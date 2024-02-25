ffd75388061df84feb7200c91d7a86bdca08f901
ruixing <lvruixing@126.com>
2024-02-25 21:57:42 +0800

    delete lock

--- client/Index.jsx
+++ client/Index.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Page = () => {
+  return (
+    <>
+      <h1 style={{ margin: 0 }}>Hello wold</h1>
+      <div style={{ position: 'absolute', width: '100%', bottom: '100px' }}>
+        <input style={{ display: 'block' }} />
+        <div>page footer</div>
+        <div>page footer</div>
+        <div>page footer</div>
+        <div>page footer</div>
+        <div>page footer</div>
+      </div>
+    </>
+  )
+}
+
 ReactDOM.render(
-  <h1>Hello wold</h1>,
+  <Page />,
   document.getElementById('content-body')
 );
